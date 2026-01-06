2d42886a0fbaf330f63cb7f83f905af6582192c7
Matthew Telford <163449269+Matthew-Telford@users.noreply.github.com>
2026-01-06 18:01:43 +0000

    Added shadcn component library and tailwind

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,21 +1,19 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-import path from 'path';
+import tailwindcss from "@tailwindcss/vite";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-    base:
-        mode == "development"
-            ? "http://localhost:5173/"
-            : "/static/api/spa/",
-    build: {
-        emptyOutDir: true,
-        outDir: "../api/static/api/spa",
+  base: mode == "development" ? "http://localhost:5173/" : "/static/api/spa/",
+  build: {
+    emptyOutDir: true,
+    outDir: "../api/static/api/spa",
+  },
+  plugins: [vue(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
     },
-    plugins: [vue()],
-    resolve: {
-        alias: {
-            '@': path.resolve(__dirname, './src'),
-        },
-    },
+  },
 }));
