c3fda462cb3cec369fb03e84ead9af62a85bc5e7
Matthew Telford <163449269+Matthew-Telford@users.noreply.github.com>
2026-01-09 16:54:12 +0000

    Started setting up the custom user model and authentication

--- api/views.py
+++ api/views.py
@@ -1,6 +1,15 @@
+from django.contrib.auth import authenticate, login
 from django.http import HttpResponse, HttpRequest, JsonResponse
 from django.shortcuts import render
 
 
 def main_spa(request: HttpRequest) -> HttpResponse:
-    return render(request, 'api/spa/index.html', {})
+    return render(request, "api/spa/index.html", {})
+
+
+def user_login(request):
+    email = request.POST["email"]
+    password = request.POST["password"]
+    user = authenticate(request, username=email, password=password)
+    if user is not None:
+        login(request, user)
